9a9ef0bc7b10b04aba43b8a1cbe84d6df51c9fcc
tarek-tj <tarekTJ@coursera-somthing.com>
2020-07-26 10:21:10 +0300

    module1 solution

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -5,36 +5,44 @@ angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', LunchCheckMessage);
 
-LunchCheckController.$inject = [$scope, $filter];
+LunchCheckController.$inject = [$scope];
 
-function LunchCheckMessage($scope, $filter){
+function LunchCheckMessage($scope){
   $scope.meals="";
   $scope.message= "";
 
   $scope.checkMeals = function (){
-    var msg = $scope.meals.split(",");
-
-    if (msg.length <=3)  {
-      if (msg.length == 1 && msg[0] == "" )
-      {
-        $scope.message ="Please enter data first";
-      }
-      else {
-        $scope.message = "Enjoy!";
-      }
-
+    var items = calculateItems($scope.meals);
+    if (items == 0)
+    {
+      $scope.message ="Please enter data first";
     }
-
+    else if (items <= 3)
+    {
+      $scope.message = "Enjoy!";
+    }
     else {
       $scope.message = "Too Much!";
     }
+
   };
-
-
-
-
 }
 
-
+function calculateItems(string){
+  var itemsNumbers = 0;
+  var items = string.split(',');
+  if (items.leghth==1 && items[0]=="") {
+    return itemsNumbers;
+  }
+  else {
+    for (var i = 0; i < items.length; i++) {
+      if (items[i]!="")
+      {
+        itemsNumbers++;
+      }
+    }
+    return itemsNumbers;
+        }
+}
 
 })();
